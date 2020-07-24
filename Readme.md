ff07772d2125c0bc24c7cd5553cafc74f1b587f8
Michael Muré <batolettre@gmail.com>
2020-07-24 16:04:20 +0200

    implement -wait to wait for refresh completion

--- Readme.md
+++ Readme.md
@@ -11,7 +11,7 @@
 - `region` - The AWS region. Default to us-east-1.
 - `warmup` - How long the new instances need to warm up. Default to the value for the health check grace period defined for the group.
 - `minhealthy` - The minimum percentage capacity to retain during the recycling. Default to 100%.
-
+- `wait` - Wait until the refresh is complete to return.
 ## Notes
 
 This feature already exist in the AWS CLI tool but only on the V2 version which is not commonly packaged at the moment. The UX is also terrible (it reads a JSON file instead of using flags). Once those problems are resolved, this could possibly be retired.
--- asg-instance-refresh.go
+++ asg-instance-refresh.go
@@ -4,9 +4,9 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/autoscaling"
 )
@@ -15,6 +15,7 @@ var (
 	region     = flag.String("region", "us-east-1", "The AWS region. Default to us-east-1.")
 	warmup     = flag.Int64("warmup", -1, `How long the new instances need to warm up. Default to the value for the health check grace period defined for the group.`)
 	minHealthy = flag.Int64("minhealthy", 100, `The minimum percentage capacity to retain during the recycling. Default to 100%.`)
+	wait       = flag.Bool("wait", false, "Wait until the refresh is complete to return.")
 )
 
 func main() {
@@ -45,25 +46,53 @@ func main() {
 
 	result, err := svc.StartInstanceRefresh(input)
 	if err != nil {
-		if aerr, ok := err.(awserr.Error); ok {
-			switch aerr.Code() {
-			case autoscaling.ErrCodeLimitExceededFault:
-				fmt.Println(autoscaling.ErrCodeLimitExceededFault, aerr.Error())
-			case autoscaling.ErrCodeResourceContentionFault:
-				fmt.Println(autoscaling.ErrCodeResourceContentionFault, aerr.Error())
-			case autoscaling.ErrCodeInstanceRefreshInProgressFault:
-				fmt.Println(autoscaling.ErrCodeInstanceRefreshInProgressFault, aerr.Error())
-			default:
-				fmt.Println(aerr.Error())
-			}
-		} else {
-			// Print the error, cast err to awserr.Error to get the Code and
-			// Message from an error.
-			fmt.Println(err.Error())
-		}
+		fmt.Println(err.Error())
 		// exit with error-ish code
 		os.Exit(1)
 	}
 
-	fmt.Printf("Refresh started on ASG %s with request ID %s\n", asg, *result.InstanceRefreshId)
+	refreshId := *result.InstanceRefreshId
+
+	fmt.Printf("Refresh started on ASG %s with request ID %s\n", asg, refreshId)
+
+	if !*wait {
+		return
+	}
+
+	start := time.Now()
+	for {
+		time.Sleep(5 * time.Second)
+
+		result, err := svc.DescribeInstanceRefreshes(&autoscaling.DescribeInstanceRefreshesInput{
+			AutoScalingGroupName: aws.String(asg),
+			InstanceRefreshIds:   []*string{result.InstanceRefreshId},
+		})
+		if err != nil {
+			fmt.Println(err.Error())
+			os.Exit(1)
+		}
+
+		if len(result.InstanceRefreshes) != 1 {
+			fmt.Printf("Found %v refresh with ID %v\n", len(result.InstanceRefreshes), refreshId)
+			os.Exit(1)
+		}
+
+		refresh := result.InstanceRefreshes[0]
+		switch *refresh.Status {
+		case "Successful":
+			fmt.Printf("Refresh completed in %v\n", refresh.EndTime.Sub(*refresh.StartTime).Truncate(time.Second))
+			return
+
+		case "Pending", "InProgress":
+			fmt.Printf("%v\tRefresh is %v\n", time.Since(start).Truncate(time.Second), *refresh.Status)
+
+		case "Failed", "Cancelling", "Cancelled":
+			fmt.Printf("Refresh is %v\n", *refresh.Status)
+			os.Exit(1)
+
+		default:
+			fmt.Printf("Unknown refresh status %v\n", *refresh.Status)
+			os.Exit(1)
+		}
+	}
 }
